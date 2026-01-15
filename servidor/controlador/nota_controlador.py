95be0a4b46ca9ab046f0741702d5956bcafff376
Sebastian Ortega <sebawarcry@gmail.com>
2026-01-15 12:44:09 -0300

    crear controlador  Nota: listar, sincronizar y  eliminar

---
+++ servidor/controlador/nota_controlador.py
@@ -0,0 +1,34 @@
+from flask import jsonify, request, abort
+from modelo.nota import Nota
+
+def listar_notas():
+    return jsonify([vars(n) for n in Nota.listar()]), 200
+
+def sincronizar_notas():
+    notas_cliente = request.json
+    if not isinstance(notas_cliente, list):
+        abort(400)
+  
+    ids_cliente = {n["id"] for n in notas_cliente}
+    
+    notas_servidor = Nota.listar()
+    for nota in notas_servidor[:]:  
+        if nota.id not in ids_cliente:
+            Nota.eliminar(nota.id)
+    
+    for n in notas_cliente:
+        Nota.guardar(
+            Nota(
+                contenido=n["contenido"],
+                updated_at=n["updated_at"],
+                _id=n["id"]
+            )
+        )
+    return jsonify([vars(n) for n in Nota.listar()]), 200
+
+def eliminar_nota(nota_id):
+    Nota.eliminar(nota_id)
+    return "", 204
+
+def estado():
+    return jsonify({"online": True}), 200
