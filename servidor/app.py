be49535cb25e2e31c757791c9bb9e6810671008b
Sebastian Ortega <sebawarcry@gmail.com>
2026-01-15 12:46:31 -0300

    Actualizar app.py: agregar rutas de API y controlador de notas

--- servidor/app.py
+++ servidor/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, send_from_directory
+from controlador import nota_controlador
 from flask_cors import CORS
 import os
 
@@ -9,11 +10,11 @@ CORS(app)
 def index():
     return send_from_directory('../cliente', 'index.html')
 
-# Endpoint para verificar conexión
-@app.route("/status")
-def status():
-    return jsonify({"online": True}), 200
 
-if __name__ == "__main__":
+app.add_url_rule('/notas', 'listar_notas', nota_controlador.listar_notas)
+app.add_url_rule('/sync', 'sincronizar', nota_controlador.sincronizar_notas, methods=['POST'])
+app.add_url_rule('/notas/<nota_id>', 'eliminar', nota_controlador.eliminar_nota, methods=['DELETE'])
+app.add_url_rule('/status', 'estado', nota_controlador.estado)
+
+if __name__ == '__main__':
     app.run(port=5000, debug=True)
-"""(devulve "GET / HTTP/1.1" 200 - funcionando correctamente)"""
